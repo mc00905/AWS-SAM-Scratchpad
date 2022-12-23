6dd1847345e7d93078fe494e89a988ed6e14526b
mc00905 <matthew.cooper6@btinternet.com>
2022-12-23 04:29:04 +0000

    move controller class instantiation to lambda environment and not indiv. handlers
    Signed-off-by: mc00905 <matthew.cooper6@btinternet.com>

--- application/src/service-layer/lambda-handlers/ProductHandler.ts
+++ application/src/service-layer/lambda-handlers/ProductHandler.ts
@@ -1,17 +1,16 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
 import { ProductController } from '../controllers/ProductController';
 
+const controller = new ProductController();
+
 export const createProduct: APIGatewayProxyHandler = async (event) => {
-    const controller = new ProductController();
     return controller.createProduct(event);
 };
 
 export const getProduct: APIGatewayProxyHandler = async (event) => {
-    const controller = new ProductController();
     return controller.getProduct(event);
 };
 
 export const deleteProduct: APIGatewayProxyHandler = async (event) => {
-    const controller = new ProductController();
     return controller.deleteProduct(event);
 };
