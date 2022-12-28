641a970d45b320809dce5e031b99f75653f1b373
mc00905 <matthew.cooper6@btinternet.com>
2022-12-28 00:15:07 +0000

    Fetch s3 object from step function task

--- application/src/state-machines/tasks/UploadJSONToBucket.ts
+++ application/src/state-machines/tasks/UploadJSONToBucket.ts
@@ -1,18 +1,33 @@
 import { Handler } from 'aws-lambda';
-import { PutObjectCommand, PutObjectCommandInput, S3Client } from '@aws-sdk/client-s3';
+import {
+    GetObjectCommand,
+    GetObjectCommandInput,
+    PutObjectCommand,
+    PutObjectCommandInput,
+    S3Client,
+} from '@aws-sdk/client-s3';
 import { v4 as uuidv4 } from 'uuid';
 
 const s3Client = new S3Client({ region: process.env.AWS_REGION });
-const BUCKET_NAME = process.env.BUCKET_NAME || 'BUCKET_NAME';
 const ACCESS_POINT_ARN = process.env.ACCESS_POINT_ARN || 'ACCESS_POINT_ARN';
 
 export const uploadJSONToBucket: Handler = async (event, context) => {
+    const key = uuidv4();
     const bucketParams: PutObjectCommandInput = {
         Bucket: ACCESS_POINT_ARN,
-        Key: uuidv4(),
+        Key: key,
         Body: JSON.stringify({
             message: 'Hello World!',
         }),
     };
     await s3Client.send(new PutObjectCommand(bucketParams));
+    const getFromBucketParams: GetObjectCommandInput = {
+        Bucket: ACCESS_POINT_ARN,
+        Key: key,
+    };
+    const obj = await s3Client.send(new GetObjectCommand(getFromBucketParams));
+    const body = (await obj.Body?.transformToString()) || undefined;
+    return {
+        result: body,
+    };
 };
